96c66b11796bd1eef1494ea40ef5e7ddf131e750
Max Beizer <max.beizer@gmail.com>
2023-12-11 11:31:28 -0600

    mix new ex_post_facto

---
+++ lib/ex_post_facto.ex
@@ -0,0 +1,18 @@
+defmodule ExPostFacto do
+  @moduledoc """
+  Documentation for `ExPostFacto`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExPostFacto.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_post_facto_test.exs
@@ -0,0 +1,8 @@
+defmodule ExPostFactoTest do
+  use ExUnit.Case
+  doctest ExPostFacto
+
+  test "greets the world" do
+    assert ExPostFacto.hello() == :world
+  end
+end
