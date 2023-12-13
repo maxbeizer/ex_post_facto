ffe809897eb3ec3a4a26fd5ab7b147531b249e59
Max Beizer <max.beizer@gmail.com>
2023-12-13 09:12:44 -0600

    Add some todos

--- lib/ex_post_facto/result.ex
+++ lib/ex_post_facto/result.ex
@@ -3,6 +3,38 @@ defmodule ExPostFacto.Result do
   The result contains the output of applying a strategy to a set of data.
   """
 
+  # TODOs:
+  # - make this concurrent
+  # - fill out data similar to backtesting output
+  # Start                     2004-08-19 00:00:00
+  # End                       2013-03-01 00:00:00
+  # Duration                   3116 days 00:00:00
+  # Exposure Time [%]                       94.27
+  # Equity Final [$]                     68935.12
+  # Equity Peak [$]                      68991.22
+  # Return [%]                             589.35
+  # Buy & Hold Return [%]                  703.46
+  # Return (Ann.) [%]                       25.42
+  # Volatility (Ann.) [%]                   38.43
+  # Sharpe Ratio                             0.66
+  # Sortino Ratio                            1.30
+  # Calmar Ratio                             0.77
+  # Max. Drawdown [%]                      -33.08
+  # Avg. Drawdown [%]                       -5.58
+  # Max. Drawdown Duration      688 days 00:00:00
+  # Avg. Drawdown Duration       41 days 00:00:00
+  # # Trades                                   93
+  # Win Rate [%]                            53.76
+  # Best Trade [%]                          57.12
+  # Worst Trade [%]                        -16.63
+  # Avg. Trade [%]                           1.96
+  # Max. Trade Duration         121 days 00:00:00
+  # Avg. Trade Duration          32 days 00:00:00
+  # Profit Factor                            2.13
+  # Expectancy [%]                           6.91
+  # SQN                                      1.78
+  # _strategy              SmaCross(n1=10, n2=20)
+
   defstruct data_points: [],
             is_position_open: false,
             starting_balance: 0.0,
